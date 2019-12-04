28606eaef1aaa30bc35c29ebed7e12e6914419b2
Jürgen Huber <jue.huber@gmail.com>
2019-12-04 13:35:22 +0100

    fix import

--- src/test/java/aoc4/SecureContainerTest.java
+++ src/test/java/aoc4/SecureContainerTest.java
@@ -4,8 +4,8 @@ import com.huber.aoc04.SecureContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SecureContainerTest {
     private SecureContainer secureContainer;
